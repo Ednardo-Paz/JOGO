e82e87a6fed26e0b716a70c19416bab1391fdad5
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-04-28 10:45:23 -0300

    Finalizado.
    finalizado.

--- js/interface.js
+++ js/interface.js
@@ -1,34 +1,117 @@
-document.addEventListener(`DOMContentLoaded`, ()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
-    let squeres = document.querySelectorAll(`.squere`);
+    let squares = document.querySelectorAll(".square");
+    let botao = document.getElementById("btn");
+    let botaoOkModal = document.getElementById("btnOK");
 
-    squeres.forEach((squere)=>{
-        squere.addEventListener(`click`, handleClick);
-
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
     })
-     
 
-})
-function handleClick(event){
-     let squere = event.target;
-     let position = squere.id;
+    botao.addEventListener('click', btnRecomecar);
+    botaoOkModal.addEventListener('click', btnCloseModal);
 
-    if( handleMove(position)) {
-        alert(`O jogo acabou.`)
-    };
-     updateSqueres(); 
+});
+
+function btnCloseModal() {
+    document.getElementById('modal').style.display = "none";
+    btnRecomecar();
 }
 
-function updateSqueres(){
-    let squeres = document.querySelectorAll(`.squere`);
-    squeres.forEach((squere)=> {
-        let position = squere.id;
-        let symbols = board[position];
+function btnRecomecar() {
+    iniciaVariaveis();
+    squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
 
-        if(symbols != ''){
-            squere.innerHTML = `<div class='${symbols}'></div>`
+        let position = square.id;
+        let symbol = board[position];
+
+        if (symbol == '') {
+            square.innerHTML = `<div class=''></div>`;
+        }
+    });
+}
+
+function handleClick(event) {
+
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)) {
+
+        showLineWin();
+
+    }
+
+    updateSquare(position);
+}
+
+function showLineWin() {
+    let linkImg = "";
+
+    //Posição da posição da imagem
+    switch (positionWin[3]) {
+
+        case "d1": {
+            linkImg = "./img/icons/linhaDiagonal1.png";
+
+            break;
         }
 
+        case "d2": {
+            linkImg = "./img/icons/linhaDiagonal2.png";
+            break;
+        }
 
-    })
+        case "v": {
+            linkImg = "./img/icons/linhaVertical.png";
+            break;
+        }
+
+        case "h": {
+            linkImg = "./img/icons/linhaHorizontal.png";
+            break;
+        }
+
+    }
+
+    setTimeout(() => {
+        for (let posWin of positionWin) {
+
+            let square = document.getElementById(posWin.toString());
+            img = document.createElement('img');
+            img.setAttribute("src", linkImg);
+            img.setAttribute("width", "100px");
+            img.setAttribute("height", "100px");
+            square.appendChild(img);
+        }
+    }, 30);
+
+    setTimeout(() => {
+         mostraTelaVitoria(positionWin[0]);
+    }, 100); 
+}
+
+function mostraTelaVitoria(posWin) {
+    document.getElementById('modal').style.display = "block";
+    addEmojiWinner(posWin);
+}
+
+function updateSquare(position) {
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+
+    if (symbol != '') {
+        square.innerHTML = `<div class='${symbol}'></div>`;
+    }
+}
+
+function addEmojiWinner(posWin) {
+    let win = document.getElementById("winner");
+    let symbol = board[posWin];
+    if (symbol == 'o') {
+        win.innerHTML = `<img class="circulo" src="./img/icons/circle.png" alt="">`;
+    } else if (symbol == 'x') {
+        win.innerHTML = `<img class="xis" src="./img/icons/xis.png" alt="">`;
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,58 +1,72 @@
 // Iniciar minhas variaveis     
-let board = ["", "", "", "", "", "", "", "", ""];
+
+let board = ['', '', '', '', '', '', '', '', ''];
 let playerTime = 0;
 let symbols = ['o', 'x'];
 let gameOver = false;
+ 
+
+let winStates = [
+    [0, 1, 2, 'h'],
+    [3, 4, 5, 'h'],
+    [6, 7, 8, 'h'],
+    [0, 3, 6, 'v'],
+    [1, 4, 7, 'v'],
+    [2, 5, 8, 'v'],
+    [0, 4, 8, 'd1'],
+    [2, 4, 6, 'd2']
+]
+let positionWin = ['', '', '', ''];
+
+function iniciaVariaveis(){
+    board = ['', '', '', '', '', '', '', '', ''];    
+    let positionWin = ['', '', '', ''];
+    playerTime = 0;
+    gameOver = false;
+}
+
 function handleMove(position) {
 
-
-
-
-  /*   if (gameOver) {
+    if (gameOver) {
         return;
-    } */
+    }
 
-
-
-
+    //Não deixa add se caso nessa posição já tiver um symbol
     if (board[position] == '') {
         board[position] = symbols[playerTime];
 
-        gameOver = isWin()
+        gameOver = isWin();
 
-        if(gameOver == false)
-        if (playerTime == 0) {
-            playerTime = 1;
-        } else {
-            playerTime = 0;
+        if (!gameOver) {
+
+            playerTime = (playerTime == 0) ? 1 : 0;
+
         }
     }
-   /*  return gameOver;
+
+    return gameOver;
+
 }
-function isWin(){
-    let winStates =[
-        [0,1,2]
-        [3,4,5]
-        [6,7,8]
-        [0,3,6]
-        [1,4,7]
-        [2,5,8]
-        [0,4,8]
-        [2,4,6]
-    ]
-    for( let i=0; i < winStates.length; i++ ){
+
+function isWin() {
+
+    for (let i = 0; i < winStates.length; i++) {
         let seq = winStates[i];
 
-        let pos1 = seq[0]; 
+        let pos1 = seq[0];
         let pos2 = seq[1];
         let pos3 = seq[2];
+        let pos4 = seq[3];
 
-        if(board[pos1] == board[pos2] &&
+        if (board[pos1] == board[pos2] &&
             board[pos1] == board[pos3] &&
-            board[pos1] != ''){
-                return true;
-            }
+            board[pos1] != '') {
+            positionWin = [pos1, pos2, pos3, pos4];
+
+            return true;
+
+        }
     }
 
-    return false;*/
-} 
+    return false;
+}
